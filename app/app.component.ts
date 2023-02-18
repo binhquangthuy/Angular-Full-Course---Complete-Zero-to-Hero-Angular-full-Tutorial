4460ccda5b2fb4974aa180a55fd1cd803cbcb824
binhquangthuy <83194297+binhquangthuy@users.noreply.github.com>
2023-02-18 19:57:53 +0700

    3 files modified, 1 added, 1 deleted

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,9 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular 5';
-  example: string = `<div>this is another div <br/> i want to insert dynamically</div>`
+  example: string = `<div>this is another div <br/> i want to insert dynamically</div>`;
+  user = {
+    name: 'Binh',
+    age: 13,
+  };
 }
---
+++ app/navbar/navbar.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  template: '<h1> navbar component</h1>',
+})
+export class NavbarComponent {}
